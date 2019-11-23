766ff0030c21cd7635adbd410126178727beb47f
Stefan Natter <stefan@natter.at>
2019-11-23 15:43:05 +0100

    docs(example): update react tool example with proper click handler

--- src/__stories__/custom-plugin-react.tsx
+++ src/__stories__/custom-plugin-react.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-console */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 type EditorJSAPI = import('@editorjs/editorjs').API
@@ -12,17 +12,33 @@ type DataType = {
 }
 
 export const Button = (props: any) => {
-  const { data, onChange } = props
+  const { api, data, onChange } = props
   const [counter, setCounter] = useState((data && data.counter) || 0)
+  const buttonRef = useRef()
 
-  const onClick = () => {
+  const onClick = useCallback(() => {
     const newCounter = counter + 1
     setCounter(newCounter)
     onChange({ counter: newCounter })
-  }
+  }, [counter, onChange])
+
+  useEffect(() => {
+    const buttonElement = buttonRef.current
+
+    api.listeners.on(buttonElement, 'click', onClick)
+
+    return () => {
+      api.listeners.off(buttonElement, 'click', onClick)
+    }
+  }, [api.listeners, onClick])
 
   return (
-    <button onClick={onClick} type="button" style={{ padding: 10 }}>
+    <button
+      ref={buttonRef}
+      onClick={onClick}
+      type="button"
+      style={{ padding: 10 }}
+    >
       Click me [clicked: {counter} times]
     </button>
   )
